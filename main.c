66d6004bedd2316cdcac133b1a1352bb942f0b88
ilarisahi <ilari.sahi@student.lut.fi>
2017-01-07 04:55:22 -0800

    commenting and cleanup

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/**
+* CT30A3370 Käyttöjärjestelmät ja systeemiohjelmointi
+* Systeemiohjelmointi-osan harjoitustyö
+* 
+* Tekijät:
+* Petri Rämö, opiskelijanumero: 0438578
+* Ilari Sahi, opiskelijanumero: 0438594
+*
+* 7.1.2017
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,14 +21,11 @@
 #define MAXNUM 40
 #define MAXLEN 160
 
+/* handles signals (user interrupt) */
 void sighandler(int sig) {
 	char dir[1024];
 	switch (sig) {
-		case SIGALRM:
-			printf("\nautologout\n");
-			exit(0);
-		case SIGINT:
-			printf("\nuser interrupt\n");			
+		case SIGINT:			
 			break;
 		default:
 			break;
@@ -26,36 +34,39 @@ void sighandler(int sig) {
 	getcwd(dir, sizeof(dir));
 			
 	/* print the prompt */
-	printf("$ %s > ", dir);
+	printf("\n$ %s > ", dir);
 	fflush(stdout);
 	return;
 }
 
-void pipeHandler(char **args, char **pipeArgs) {
-	/* fork to run the command */
+/* handles piping */
+void pipeHandler(char **args, char **pipeArgs) {	
 	int fd[2];
 	pid_t pid1;
 	pid_t pid2;
-		
+	
+	/* fork to run the command */
 	switch (pid1 = fork()) {
 		case -1:
 			/* error */
 			perror("fork");
 			return;
 		case 0:
-			/* child process */
 			pipe(fd);
+			/* create child and parent processes */
 			switch(pid2 = fork()) {
 				case -1:
 					perror("fork");
 					return;
 				case 0:
+					/* set output */
 					dup2(fd[1], 1);
 					close(fd[0]);
 					execvp(args[0], args);
 					perror("execvp");
 					break;
 				default:
+					/* get input */
 					waitpid(-1, NULL, 0);
 					dup2(fd[0], 0);
 					close(fd[1]);
@@ -63,21 +74,21 @@ void pipeHandler(char **args, char **pipeArgs) {
 					perror("execvp");
 			}
 		default:
+			/* wait for child (piping) to finish */
 			waitpid(-1, NULL, 0);
 	}
 }
 
 
-int main(void)
-{
+int main(void) {
 	char * cmd, line[MAXLEN], * args[MAXNUM], **pipeArgs;
 	int background, i, j, in, out, inout, p;
 	int pid;
 	char dir[1024], input[64], output[64];
 	
-	signal(SIGALRM, sighandler);
 	signal(SIGINT, sighandler);	
 	
+	/* main loop for terminal */
 	while (1) {
 		out = 0;
 		in = 0;
@@ -85,12 +96,9 @@ int main(void)
 		p = 0;
 		background = 0;
 
-		/* set the timeout for alarm signal (autologout) */
-		/*alarm(LOGOUT);*/
-		getcwd(dir, sizeof(dir));			
 		/* print the prompt */
-		printf("$ %s > ", dir);
-		
+		getcwd(dir, sizeof(dir));		
+		printf("$ %s > ", dir);		
 		
 		/* read the users command */
 		if (fgets(line,MAXLEN,stdin) == NULL) {
@@ -112,18 +120,16 @@ int main(void)
 		i = 0;
 		cmd = line;
 		while ( (args[i] = strtok(cmd, " ")) != NULL) {
-			printf("arg %d: %s\n", i, args[i]);
 			i++;
 			cmd = NULL;
 		}
-
-
 		
+		/* test for exit */
 		if (strcmp(args[0],"exit")==0) {
 			exit(0);
 		}
 
-
+		/* test for cd (go home) only */
 		if (strcmp(args[0],"cd")==0) {
 			if (args[1] == NULL) {
 				chdir(getenv("HOME"));				
@@ -133,25 +139,24 @@ int main(void)
 			continue;
 		}
 
+		/* test for redirections and pipe */
 		for (j = 0; j < i; j++) {
 			if (strcmp(args[j], ">") == 0) {
-				printf("> catched\n");
 				args[j] = NULL;
 				strcpy(output, args[j+1]);
 				out = 1;
 			} else if (strcmp(args[j], "<") == 0) {
-				printf("< catched\n");
 				args[j] = NULL;
 				strcpy(input, args[j+1]);
 				in = 1;
 			} else if (strcmp(args[j], "|") == 0) {
-				printf("| catched\n");
 				p = 1;
 				args[j] = NULL;
 				pipeArgs = args + j + 1;
 			}
 		}
 
+		/* test for multiple redirections */
 		if ((out == 1) && (in == 1)) {
 			in = 0;
 			out = 0;
@@ -166,8 +171,8 @@ int main(void)
 				continue;
 			case 0:
 				/* child process */
+				/* double redirection */
 				if (inout == 1) {
-					printf("kaksisuuntainen redirection\n");
 					FILE* file = fopen(input, "r");
 					dup2(fileno(file), 0);
 					fclose(file);
@@ -175,32 +180,25 @@ int main(void)
 					dup2(fileno(file), 1);
 					fclose(file);
 				}
+				/* output redirection */
 				else if (out == 1) {
-					printf("output redirection\n");
 					FILE* file = fopen(output, "w");
 					dup2(fileno(file), 1);
 					fclose(file);
-				} else if (in == 1) {
-					printf("input redirection\n");
+				}
+				/* input redirection */
+				else if (in == 1) {
 					FILE* file = fopen(input, "r");
 					dup2(fileno(file), 0);
 					fclose(file);
-				} else if (p == 1) {
-					printf("pipe redirection\n");
-					i = 0;
-					while (args[i] != NULL) {
-						printf("arg %d: %s\n", i, args[i]);
-						i++;
-					}
-					i = 0;
-					while (pipeArgs[i] != NULL) {
-						printf("arg %d: %s\n", i, pipeArgs[i]);
-						i++;
-					}
+				}
+				/* pipe */
+				else if (p == 1) {
 					pipeHandler(args, pipeArgs);
 					exit(1);
 				}
 
+				/* execute if not pipe */
 				execvp(args[0], args);
 				perror("execvp");
 				exit(1);
@@ -208,7 +206,7 @@ int main(void)
 				/* parent (shell) */
 				if (!background) {
 					alarm(0);
-					//waitpid(pid, NULL, 0);
+					/* wait for children to finish */
 					while (wait(NULL)!=pid)
 						printf("some other child process exited\n");
 				}
